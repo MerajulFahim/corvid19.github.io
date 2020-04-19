feaf2ba641758acf3633e5e14850a7ffd73184c3
MerajulFahim <61354243+MerajulFahim@users.noreply.github.com>
2020-04-19 19:16:58 +0600

    Updates

---
+++ precache-manifest.151398b6b71f9576b0e62551ebc6148b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d13e8177cb28c32cb7018fca0b166ff",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "45336e24b96c48661d5c",
+    "url": "/covid-19/static/css/main.aa25077b.chunk.css"
+  },
+  {
+    "revision": "023e7fe3749f250bc29b",
+    "url": "/covid-19/static/js/2.fd35c4ac.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.fd35c4ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45336e24b96c48661d5c",
+    "url": "/covid-19/static/js/main.5d65a748.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
